fa0be3b522c83d47a74ab221c4953ed7f25a1d10
SergeWilfried <willytheigeek@gmail.com>
2026-07-18 07:21:20 +0000

    feat: initial commit

---
+++ src/theme.ts
@@ -0,0 +1,27 @@
+export const colors = {
+  red: '#D71A28',
+  redDark: '#B8121F',
+  redTint: '#FBF1F2',
+  redTintBorder: '#F2D9DB',
+  ink: '#1E262E',
+  text: '#3E4753',
+  textMuted: '#5A6976',
+  textFaint: '#7A8593',
+  border: '#E3E7EB',
+  borderLight: '#E9EDF1',
+  borderLighter: '#EEF1F4',
+  panelBg: '#FBFBFC',
+  sectionBg: '#F7F8FA',
+  dark: '#1D1D1B',
+  darkText: '#EAEAEA',
+  darkTextMuted: '#B9BDC1',
+  darkTextFaint: '#9DA2A7',
+  darkTextFainter: '#8A8F94',
+} as const;
+
+export const fonts = {
+  heading: "'Barlow', sans-serif",
+  body: "'Open Sans', sans-serif",
+} as const;
+
+export const maxWidth = 1280;
